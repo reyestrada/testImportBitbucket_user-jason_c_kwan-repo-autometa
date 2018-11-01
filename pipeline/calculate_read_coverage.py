5a6ce65b12687c6d52bf460742932b495a99bcbe
Evan Rees <erees@wisc.edu>
2018-11-01 09:26:01 -0500

    reformatted for readability and added more robust file path handling. Changed some argparse flags for more concise input. changed file checking and command input string handling. deleted comment to bioinformatics server file location and miscellaneous superfluous comments. Changed/added stdout/logging information.

--- pipeline/calculate_read_coverage.py
+++ pipeline/calculate_read_coverage.py
@@ -25,10 +25,10 @@ import os
 #Input: fastq (or fastq.gz?) reads, metagenomic assembly contigs as fasta file
 #Output: "contig\tread_coverage"
 
-#See: /home/ijmiller/slurm_scripts/2016/October/7-Oct-16_Bpac5Tips_coverage_calculation.sh
-#Dependencies: bowtie2 (2.2.5), samtools (0.1.18), \
-    #Bedtools (genomeCoverageBed: v2.17.0), contig_coverage_from_bedtools.pl,\
-    #fasta_length_table.pl, python modules: suprocess, argparse
+#: Dependencies: bowtie2 (2.2.5), samtools (0.1.18),
+#: Bedtools (genomeCoverageBed: v2.17.0)
+#: contig_coverage_from_bedtools.pl
+#: fasta_length_table.pl
 
 #1. Align the reads to your assembly with bowtie2.
 #2. Convert the SAM file to a sorted BAM file, and create a BAM index.
@@ -37,21 +37,9 @@ import os
 #num_cpus = multiprocessing.cpu_count()
 #use_cpus = int(round((num_cpus / 2)))
 
-#argument parser
-parser = argparse.ArgumentParser(description='Script to tabulate paired-end or single read \
-coverage using Samtools and Bedtools. Note: it is recommended that you quality \
-filter your *.fastq or *fastq.gz files prior to calculating read coverage.')
-parser.add_argument('-a','--assembly', metavar='<assembly.fasta>', help='Path to input assembly file', required=True)
-parser.add_argument('-p','--processors', metavar='<int>', help='Number of processors', type=int, default=1)
-parser.add_argument('-F','--forward_reads', metavar='<reads.fastq|reads.fastq.gz>', help='Paired (*.fastq|*.fastq.gz) forward reads (must be in same order as reverse list)', nargs='*')
-parser.add_argument('-R','--reverse_reads', metavar='<reads.fastq|reads.fastq.gz>', help='Paired (*.fastq|*.fastq.gz) reverse reads (must be in same order as forward list)', nargs='*')
-parser.add_argument('-S','--single_reads', metavar='<reads.fastq|reads.fastq.gz>', help='Single (*.fastq|*.fastq.gz) reads', nargs='*')
-parser.add_argument('-o','--output_dir', metavar='<dir>', help='Path to output directory', default='.')
-args = vars(parser.parse_args())
-
-def run_command(command_string, stdout_path = None):
-    # Function that checks if a command ran properly. If it didn't, then print an error message then quit
-    print('calculate_read_coverage.py, run_command: ' + command_string)
+def run_cmd(command_string, stdout_path = None):
+    # Function checks if command ran properly. If not print error message & quit
+    print('calculate_read_coverage.py\ncommand: ' + command_string)
     if stdout_path:
         f = open(stdout_path, 'w')
         exit_code = subprocess.call(command_string, stdout=f, shell=True)
@@ -60,112 +48,140 @@ def run_command(command_string, stdout_path = None):
         exit_code = subprocess.call(command_string, shell=True)
 
     if exit_code != 0:
-        print('calculate_read_coverage.py: Error, the command:')
+        print('calculate_read_coverage.py\nError!\ncommand:')
         print(command_string)
-        print('failed, with exit code ' + str(exit_code))
+        print('Failed!\nExit code: ' + str(exit_code))
         exit(1)
 
-def run_bowtie2(path_to_assembly,paths_to_F_reads,paths_to_R_reads,paths_to_S_reads,num_processors=1):
+def run_bowtie2(asm_fpath, f_reads, r_reads, s_reads, num_cpus=1):
     # Check the number/type of reads we have as input
-    if not (len(paths_to_F_reads) or len(paths_to_R_reads) or len(paths_to_S_reads)):
-        print ('Error, you need to specify at least one read file')
+    if not (len(f_reads) or len(r_reads) or len(s_reads)):
+        print ('Error! Specify at least one read file')
         quit()
 
-    if not (len(paths_to_F_reads) == len(paths_to_R_reads)):
-        print('Error, you need to specify the same number of forward and reverse read files')
+    if not (len(f_reads) == len(r_reads)):
+        print('Error! Specify the same number of forward and reverse read files')
         quit()
 
-    if paths_to_F_reads:
-        F_read_path_string = ','.join(paths_to_F_reads)
-        R_read_path_string = ','.join(paths_to_R_reads)
+    if f_reads:
+        f_read_path_str = ','.join(f_reads)
+        r_read_path_str = ','.join(r_reads)
 
-    if paths_to_S_reads:
-        S_read_path_string = ','.join(paths_to_S_reads)
+    if s_reads:
+        s_read_path_str = ','.join(s_reads)
 
 	#When "shell = True", need to give one string, not a list
     #Build bowtie2 database
-    assembly_filename = os.path.abspath(path_to_assembly).split('/')[-1]
-    bowtie2_db = output_dir + '/' + assembly_filename
-    run_command(" ".join(['bowtie2-build ',path_to_assembly, bowtie2_db]))
+    assembly_fname, _ = os.path.splitext(os.path.basename(asm_fpath))
+    bowtie2_db = '{}/{}'.format(output_dir, assembly_fname)
+    cmd = ' '.join(['bowtie2-build', asm_fpath, bowtie2_db])
+    run_cmd(cmd)
     #Run bowtie2 alignment
-    sam_file_name = bowtie2_db + '.sam'
+    sam_fname = bowtie2_db + '.sam'
+    bt2_cmd_list = [
+        'bowtie2 -x', bowtie2_db,
+        '-q --phred33 --very-sensitive --no-unal',
+        '-p', str(num_cpus),
+        '-S', sam_fname
+        ]
 
-    bowtie2_command_string = 'bowtie2 -x ' + bowtie2_db + ' -q --phred33 --very-sensitive --no-unal -p ' + str(num_processors) + ' -S ' + sam_file_name
+    if f_reads:
+        bt2_cmd_list.extend(['-1', f_read_path_str, '-2', r_read_path_str])
 
-    if paths_to_F_reads:
-        bowtie2_command_string = bowtie2_command_string + ' -1 ' + F_read_path_string
-        bowtie2_command_string = bowtie2_command_string + ' -2 ' + R_read_path_string
+    if s_reads:
+        bt2_cmd_list.extend(['-U', s_read_path_str])
 
-    if paths_to_S_reads:
-        bowtie2_command_string = bowtie2_command_string + ' -U ' + S_read_path_string
+    bt2_cmd = ' '.join(bt2_cmd_list)
+    run_cmd(bt2_cmd)
+    return sam_fname
 
-    run_command(bowtie2_command_string)
-    return sam_file_name
+
+#argument parser
+parser = argparse.ArgumentParser(description='Script to tabulate paired-end or \
+single read coverage using Samtools and Bedtools. Note: it is recommended that \
+you quality filter your *.fastq or *fastq.gz files prior to calculating read \
+coverage.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-a','--assembly', metavar='<assembly.fasta>',
+help='Path to input assembly file', required=True)
+parser.add_argument('-p','--processors', metavar='<int>', type=int, default=1,
+help='Number of processors')
+parser.add_argument('-fwd','--fwd_reads', metavar='<PE_F_reads>',  nargs='*',
+help='Paired (*.fastq|*.fastq.gz) forward reads (must be in same order as reverse list)')
+parser.add_argument('-rev','--rev_reads', metavar='<PE_R_reads>', nargs='*',
+help='Paired (*.fastq|*.fastq.gz) reverse reads (must be in same order as forward list)')
+parser.add_argument('-S','--single_reads', metavar='<SE_reads>',
+help='Single (*.fastq|*.fastq.gz) reads', nargs='*')
+parser.add_argument('-o','--output_dir', metavar='<dir>', default=os.getcwd(),
+help='Path to output directory')
+
+args = vars(parser.parse_args())
 
 #Check for dependencies in $PATH
 
 #1. Align the comparison dataset reads to your assembly with bowtie2.
-assembly_file = args['assembly']
-forward_read_path_list = args['forward_reads']
-reverse_read_path_list = args['reverse_reads']
-single_read_path_list = args['single_reads']
+assembly_fpath = os.path.abspath(args['assembly'])
+assembly_fname, _ = os.path.splitext(os.path.basename(assembly_fpath))
+read_paths = {
+    'fwd':args['fwd_reads'],
+    'rev':args['rev_reads'],
+    'single': args['single_reads']
+}
 
-output_dir = args['output_dir']
+output_dir = os.path.abspath(args['output_dir'])
 
-if not os.path.isfile(assembly_file):
-    print ('Error! Could not find assembly file at the following path: ' + assembly_file)
+if not os.path.isfile(assembly_fpath):
+    print('Error! Assembly path not found.\nPath: {}'.format(assembly_fpath))
     exit(1)
 
-assembly_filename = os.path.abspath(assembly_file).split('/')[-1]
-
 # Check that at least one of the above lists is not empty
-if not (forward_read_path_list or reverse_read_path_list or single_read_path_list):
-    print('Error! You need to specify some reads, with -F/-R (paired) and/or -S (single)')
+if not (read_paths['fwd'] or read_paths['rev'] or read_paths['single']):
+    print('Error! Must provide -fwd/-rev (paired) and/or -S (single)')
     exit(1)
 
 # Check that all read paths exist
-if forward_read_path_list:
-    concatenated_read_path_list = forward_read_path_list
-if reverse_read_path_list:
-    concatenated_read_path_list = concatenated_read_path_list + reverse_read_path_list
-if single_read_path_list and not reverse_read_path_list and not forward_read_path_list:
-    concatenated_read_path_list =  single_read_path_list
-    forward_read_path_list = []
-    reverse_read_path_list = []
-elif single_read_path_list:
-    concatenated_read_path_list = concatenated_read_path_list + single_read_path_list
-for path in concatenated_read_path_list:
-    print(path)
-    if not os.path.isfile(path):
-        print('Error! Cannot find the read file: ' + path)
-        exit(1)
+for read_type in read_paths:
+    for fpath in read_paths[read_type]:
+        print('Read type: {} Path: {}'.format(read_type, fpath))
+        if not os.path.isfile(fpath):
+            print('Error! Cannot find the read file: ' + path)
+            exit(1)
 
 # Check that the output dir exists
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
-
-sam_file = run_bowtie2(assembly_file,forward_read_path_list,reverse_read_path_list,single_read_path_list,args['processors'])
+sam_file = run_bowtie2(
+    asm_fpath = assembly_fpath,
+    f_reads = read_paths['fwd'],
+    r_reads = read_paths['rev'],
+    s_reads = read_paths['single'],
+    num_cpus = args['processors']
+)
 
 #2. Convert the SAM file to a sorted BAM file, and create a BAM index.
-sorted_bam_file = output_dir + '/' + assembly_filename + '.sort.bam'
-run_command('samtools view -bS {} | samtools sort -o {}'.format(sam_file, sorted_bam_file))
+sorted_bam_fh = '{}/{}.sort.bam'.format(output_dir, assembly_fname)
+cmd = ' '.join(['samtools view -bS', sam_file, '| samtools sort -o', sorted_bam_fh])
+run_cmd(cmd)
 
 #Clean up the SAM file, which will be a lot larger than the sorted BAM file
-run_command('rm ' + sam_file)
+os.remove(sam_file)
 
 #3. Tabulate the average coverage of each contig.
-
 #Run fasta_length_table.pl
-contig_length_tab_file = output_dir + '/' + assembly_filename + '.tab'
-run_command('fasta_length_table.pl ' + assembly_file, contig_length_tab_file)
+contig_len_tab = '{}/{}.tab'.format(output_dir, assembly_fname)
+cmd = ' '.join(['fasta_length_table.pl', assembly_fpath, contig_len_tab])
+run_cmd(cmd)
 
 #Run genomeCoverageBed
-genome_bed_file = output_dir + '/' + assembly_filename + '.txt'
-run_command('genomeCoverageBed -ibam {} -g {}'.format(sorted_bam_file, contig_length_tab_file), genome_bed_file)
+genomebed_fh = '{}/{}.txt'.format(output_dir, assembly_fname)
+cmd = ' '.join(['genomeCoverageBed',
+                '-ibam', sorted_bam_fh,
+                '-g', contig_len_tab])
+run_cmd(cmd, genomebed_fh)
 
 #Build final table
-outfile = output_dir + '/coverage.tab'
-run_command('contig_coverage_from_bedtools.pl ' + genome_bed_file, outfile)
+outfile = '{}/coverage.tab'.format(output_dir)
+cmd = ' '.join(['contig_coverage_from_bedtools.pl', genomebed_fh])
+run_cmd(cmd, outfile)
 
 #Future features: check for .fastq.gz, dependencies in path, default processors
